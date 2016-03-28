ec6eecf389d4006983657dce6c24347121e26f66
Zizheng Wu <me@zizhengwu.com>
2016-03-28 04:19:48 -0400

    the size of the universe should be noticeably larger

--- experiment.py
+++ experiment.py
@@ -1,8 +1,8 @@
 from RunBloomFilter import RunBloomFilter
 
 if __name__ == '__main__':
-    for i in range(1, 50000):
-        options = {'n': 65536, 'm': i, 'N': 100000}
+    for i in range(1, 10000, 5):
+        options = {'n': 4096, 'm': i, 'N': 10000}
         experiment = RunBloomFilter(options)
         experiment.insert()
         print('{}\t{}'.format(i, experiment.check()))
