43f9ccf61350a31c9ddd81f5ea25f316324025fa
Zizheng Wu <me@zizhengwu.com>
2016-03-28 03:25:13 -0400

    move option parser out of the class initialization

--- main.py
+++ main.py
@@ -2,23 +2,15 @@ from optparse import OptionParser
 from BloomFilter import *
 import random
 
+
 class RunBloomFilter:
-    def __init__(self):
+    def __init__(self, options):
         self.bloom_filter = None
-        parser = OptionParser()
 
-        parser.add_option('-N', dest='N', help='size of the universe', type='int', default=100000)
-        parser.add_option('-m', dest='m', help='m items into the subset S', type='int', default=10)
-        parser.add_option('-n', dest='n', help='the table size of the Bloom filter', type='int', default=500)
+        self.bloom_filter = BloomFilter(options['N'], options['m'], options['n'])
 
-        (self.options, args) = parser.parse_args()
-
-        print(self.options)
-
-        self.bloom_filter = BloomFilter(self.options.N, self.options.m, self.options.n)
-
-        self.to_be_inserted = random.sample(range(0, self.options.N), self.options.m)
-        self.should_be_negative = list(set(range(0, self.options.N)) - set(self.to_be_inserted))
+        self.to_be_inserted = random.sample(range(0, options['N']), options['m'])
+        self.should_be_negative = list(set(range(0, options['N'])) - set(self.to_be_inserted))
 
     def insert(self):
         for i in self.to_be_inserted:
@@ -32,10 +24,20 @@ class RunBloomFilter:
                 wrong += 1
             else:
                 correct += 1
-        print(wrong / (correct + wrong))
+        return(wrong / (correct + wrong))
 
 if __name__ == '__main__':
-    experiment = RunBloomFilter()
-    experiment.insert()
-    experiment.check()
+    parser = OptionParser()
 
+    parser.add_option('-N', dest='N', help='size of the universe', type='int', default=100000)
+    parser.add_option('-m', dest='m', help='m items into the subset S', type='int', default=10)
+    parser.add_option('-n', dest='n', help='the table size of the Bloom filter', type='int', default=500)
+
+    (parsed, args) = parser.parse_args()
+
+    options = {'N': parsed.N, 'm': parsed.m, 'n': parsed.n}
+    print(options)
+
+    experiment = RunBloomFilter(options)
+    experiment.insert()
+    print(experiment.check())
